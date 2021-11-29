be216d3edfac0d58c8d1e3c895ef8fca07e0f63b
Shubham Gantayat <sgantayat9@gmail.com>
2021-11-29 10:26:34 +0530

    test on aws

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import flask_monitoringdashboard as dashboard
+import os
+
+app = Flask(__name__)
+dashboard.bind(app)
+CORS(app)
+
+
+@app.route('/', methods=['GET'])
+@cross_origin()
+def home():
+    return "App is running"
+
+
+if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = int(os.getenv("PORT", 5000))
+    app.run(host=host, port=port, debug=True)
