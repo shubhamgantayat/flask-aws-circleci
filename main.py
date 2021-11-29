a0ce5b8997f5cd2154f61fe5f98fa15769f04ba6
Shubham Gantayat <sgantayat9@gmail.com>
2021-11-29 16:51:29 +0530

    test on ebs

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ CORS(app)
 @app.route('/', methods=['GET'])
 @cross_origin()
 def home():
-    return "App is running.hello world.jk"
+    return "App is running.hello world.jkhhhh"
 
 
 if __name__ == '__main__':
